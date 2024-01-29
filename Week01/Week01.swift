353642191f944b8b637b45efc30bb4306e61d063
yian6622 <yc6622@nyu.edu>
2024-01-29 15:59:40 -0500

    test

--- Week01/Week01.swift
+++ Week01/Week01.swift
@@ -1,16 +1,29 @@
 import Foundation
 
-// String of characters to be used for generating patterns
-let str = "🟥🟩🟨🌑"
+let str = "👋
+🤚
+🖐
+✋
+🖖
+👌
+🤌
+🤏
+✌️
+🤞
+🫰
+🤟
+🤘
+🤙
+🫵
+🫱"
 
-// Function to get character at specific offset in a string
 func charAt(_ str:String, _ offset:Int) -> String {
     let index = str.index(str.startIndex, offsetBy: offset)
     let char = str[index]
     return String(char)
 }
 
-// Function to generate a line of random characters of given length
+
 func generateLine(_ n:Int) {
     var nstr = ""
     for _ in 0..<n {
@@ -20,14 +33,14 @@ func generateLine(_ n:Int) {
     print(nstr)
 }
 
-// Function to generate a block of lines
+
 func generateBlock(_ n: Int) {
     for _ in 0..<n {
         generateLine(n)
     }
 }
 
-// Generate three blocks of 5 lines each
+
 generateBlock(5)
 print("")
 generateBlock(5)
