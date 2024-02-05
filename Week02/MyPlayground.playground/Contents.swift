200812ed408f38c3b21ab0e899eeefa21bc0b2f8
yian6622 <yc6622@nyu.edu>
2024-02-05 15:53:21 -0500

    molab

--- Week02/MyPlayground.playground/Contents.swift
+++ Week02/MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let path = URL(string: "https://raw.githubusercontent.com/sindresorhus/cows/main/cows.txt")
+let cowsStr = try String(contentsOf: path!, encoding: .utf8)
+print("cowsStr.count", cowsStr.count)
+
+let cowsSplit = cowsStr.split(separator: "\n", omittingEmptySubsequences: false)
+print("cowsSplit.count \(cowsSplit.count)")
+for index in 0...10 {
+  let it = cowsSplit[index]
+  print("\(it) \(it.count) \(index)")
+}
+
+// Find empty lines in cowsSplit array
+// using enumerated() tuples
+//
+for (index, value) in cowsSplit.enumerated() {
+  if value.count == 0 && index < 50 {
+    print("index \(index)")
+  }
+}
+
+// Find empty lines in cowsSplit array
+// by counting from 0 to n
+//
+let n = 50 // cowsSplit.count
+for index in 0..<n {
+  let value = cowsSplit[index]
+  if value.count == 0 {
+    print("index \(index)")
+  }
+}
+
---
+++ Week02/class.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// - what programming languages have you used? how much?  - what is your favorite mobile app and why?  - if you have a concept for a final project write a short description  [] has apple developer account  [] email apple id to jht
+
+//
